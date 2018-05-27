78d260b35f1d41a5e9d36ad35076ecbd5deba40f
Josue Zarzosa <josketres@gmail.com>
2018-05-27 20:10:02 +0200

    Add datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,16 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatFormFieldModule, MatInputModule, MatTabsModule, MatToolbarModule} from '@angular/material';
-import { NaturalPersonRfcComponent } from './natural-person-rfc/natural-person-rfc.component';
+import {
+  MatButtonModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatNativeDateModule,
+  MatTabsModule,
+  MatToolbarModule
+} from '@angular/material';
+import {NaturalPersonRfcComponent} from './natural-person-rfc/natural-person-rfc.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +28,9 @@ import { NaturalPersonRfcComponent } from './natural-person-rfc/natural-person-r
     MatToolbarModule,
     MatTabsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
